f747330348bd2a8d44279673fe5e128d49d2bdf7
O2Soft <softcircles001@gmail.com>
2022-12-28 18:13:45 +0500

    add api intigration in blogs get data from api and display on dom

--- huntingcoder/pages/blogs.js
+++ huntingcoder/pages/blogs.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import styles from "../styles/Blogs.module.css";
+import axios from "axios";
 function Blogs() {
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    // fetch("http://localhost:3000/api/blogs")
+    //   .then((res) => {
+    //     return res.json();
+    //   })
+    //   .then((parse) => {
+    //     console.log(parse);
+    //   });
+    axios
+      .get("http://localhost:3000/api/blogs")
+      .then((res) => {
+        setBlogs(res.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   return (
     <>
       <style jsx>
@@ -18,7 +37,17 @@ function Blogs() {
       </style>
       <div className={styles.container}>
         <div className={styles.blogs}>
-          <div className={styles.blogsItems}>
+          <h2>Famous Bologs</h2>
+          {blogs.map((item) => (
+            <div className={styles.blogsItems} key={item.slug}>
+              <Link href={`/blogspost/${item.slug}`}>
+                <h3>{item.title}</h3>
+              </Link>
+              <br />
+              <p>{item.content.substr(0, 140)}</p>
+            </div>
+          ))}
+          {/* <div className={styles.blogsItems}>
             <h2>Famous Bologs</h2>
             <Link href={"/blogspost/nextjs"}>
               <h3>How to Start with Next JS</h3>
@@ -35,7 +64,7 @@ function Blogs() {
             <h3>How to Start with Next JS</h3>
             <br />
             <p>Next JS is a framwork of React JS</p>
-          </div>
+          </div> */}
         </div>
       </div>
     </>
